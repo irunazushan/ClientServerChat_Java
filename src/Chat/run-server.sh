103be2a2eed78b18c689aa709b2bd13024548f32
root <root@irunazu.localdomain>
2024-07-16 22:17:53 +0300

    upd script to run Docker

--- src/Chat/run-server.sh
+++ src/Chat/run-server.sh
@@ -1,3 +1,10 @@
+#run postgreSQL db with docker
+docker build -t chat-postgres-db .
+docker run -d --name chat-postgres-container -p 5432:5432 chat-postgres-db
+
+# To check connection use:
+# psql -h localhost -U postgres -d database
+
 #build server
 cd SocketServer
 mvn clean package
@@ -5,7 +12,7 @@ mvn clean package
 #move to target folder
 cd ..
 mkdir -p target
-mv SocketServer/target/socket-server-jar-with-dependencies.jar target/socket-server.jar
+mv ./SocketServer/
 
 #run client
 java -jar target/socket-server.jar --port=8081
